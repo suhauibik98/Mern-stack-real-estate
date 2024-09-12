152424e38c1f643b1a3f35741c0ffc62855353ed
suhib okur <suhib.okur@Eranovel.local>
2024-09-12 17:41:05 +0300

    upload image

--- backend/sever.js
+++ backend/sever.js
@@ -50,3 +50,4 @@ app.use((err , req , res , next)=>{
 
 // DB_URL = "mongodb+srv://suhaib:Suhaib123456789@cluster0.a3obgc0.mongodb.net/ESTATE-MERN?retryWrites=true&w=majority&appName=Cluster0"
 // JWT_SECRET= "703e534c1cab2f3b33ec00c1bf526e81e9cfddcd3c3b3f03aa9da4ef88ec045c"
+//4:42:46
